558cc79f4fe3aa8c73256e4f64378c0d2eeb649c
Franck Tiomo <einstein-franck.tiomo-epongo@polymtl.ca>
2025-03-11 00:10:07 -0400

    Implement binary conversion for characters and add tests for getBinaire function

--- python/TP1/xor.py
+++ python/TP1/xor.py
@@ -1,5 +1,12 @@
 def getBinaire(char):
-	pass
+	""" Convertit un caractère en sa représentation binaire
+	:param char: Caractère à convertir
+  :return: Représentation binaire du caractère
+	"""
+	result = bin(ord(char))[2:]
+	if len(result) < 8:
+		result = '0'*(8-len(result)) + result
+	return result
 
 def opXor(char1, char2):
 	pass
@@ -11,7 +18,12 @@ def decryptXor(message, clef):
 	pass
 
 def testGetBinaire():
-	pass
+	assert(getBinaire('A') == '01000001')
+	assert(getBinaire('a') == '01100001')
+	assert(getBinaire(' ') == '00100000')
+	assert(getBinaire('é') == '11101001')
+	assert(getBinaire('@') == '01000000')
+	assert(getBinaire(':') == '00111010')
 
 def testOpXor():
   pass
@@ -21,3 +33,6 @@ def testCryptXor():
 
 def testDecryptXor():
 	pass
+
+
+testGetBinaire()
