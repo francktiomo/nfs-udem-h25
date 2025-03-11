24dbe912864a1389d0c20019037376a5fc1aa8cc
Franck Tiomo <einstein-franck.tiomo-epongo@polymtl.ca>
2025-03-10 23:54:45 -0400

    Create file for xor encryption

---
+++ python/TP1/xor.py
@@ -0,0 +1,23 @@
+def getBinaire(char):
+	pass
+
+def opXor(char1, char2):
+	pass
+
+def cryptXor(message, clef):
+	pass
+
+def decryptXor(message, clef):
+	pass
+
+def testGetBinaire():
+	pass
+
+def testOpXor():
+  pass
+
+def testCryptXor():
+  pass
+
+def testDecryptXor():
+	pass
